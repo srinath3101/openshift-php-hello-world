b03173faaeeec10df637fe18130d1f95101d3ef8
root <root@master1.76c2.internal>
2019-08-06 09:24:45 +0000

    this is test activity

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 
 <body>
 
-<?php echo "Hello World!"; ?>
+<?php echo "This is test box"; ?>
 <?php if($_ENV["HOSTNAME"]) {?><h3>My hostname is <?php echo $_ENV["HOSTNAME"]; ?><br /><br />
 
 <?php $links = [];
